4d049f2749fe84ea2aa28b95b175ba6124df82ed
Abhishek Tandon <web.tandon@gmail.com>
2019-10-29 18:03:29 +0530

    Updates

---
+++ precache-manifest.53ab5a2afa879d3dd5a6ea404fa3c08e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3635fef9728a6c93d60b42583f1515d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "efb91361c46affbfae73",
+    "url": "/robofriends/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "be779c89af30068e4623",
+    "url": "/robofriends/static/css/main.143be194.chunk.css"
+  },
+  {
+    "revision": "efb91361c46affbfae73",
+    "url": "/robofriends/static/js/2.650e21cc.chunk.js"
+  },
+  {
+    "revision": "be779c89af30068e4623",
+    "url": "/robofriends/static/js/main.2865a568.chunk.js"
+  },
+  {
+    "revision": "4b2f33b4d90bd66785bd",
+    "url": "/robofriends/static/js/runtime~main.6fa9ffe3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
